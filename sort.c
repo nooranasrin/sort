1f4cbae19c89675753c4d818cc33aff834939a52
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-05-22 18:20:53 +0530

    the functions related to bubble sort moved into seperate file

--- sort.c
+++ sort.c
@@ -8,22 +8,6 @@ void swap_array_elements(ArrayVoid_ptr array, int first_index, int second_index)
   array->values[second_index] = temp;
 }
 
-void perform_bubble_sort(ArrayVoid_ptr array,  Matcher is_equal) {
-  for (int i = 0; i < array->length; i++) {
-    int is_swapped = 0;
-    for (int j = i + 1; j < array->length; j++)
-    {
-      if (is_equal(array->values[j], array->values[i])) {
-        swap_array_elements(array, i, j);
-        is_swapped = 1;
-      }
-    }
-    if(!is_swapped) {
-      return;
-    }
-  }
-}
-
 void perform_insertion_sort(ArrayVoid_ptr array, Matcher is_equal) {
   for (int i = 0; i < array->length; i++)
   {
@@ -37,22 +21,6 @@ void swap_node(Node_ptr node1, Node_ptr node2) {
   Element temp = node1->element;
   node1->element = node2->element;
   node2->element = temp;
-}
-
-void perform_list_bubble_sort(List_ptr list, Matcher is_equal) {
-  Node_ptr pWalk = list->first;
-
-  while(pWalk != NULL) {
-    Node_ptr another_element = pWalk->next;
-    while(another_element != NULL) {
-      if (is_equal(another_element->element, pWalk->element))
-      {
-        swap_node(pWalk, another_element);
-      }
-      another_element = another_element->next;
-    }
-    pWalk = pWalk->next;
-  }
 }
 
 void perform_list_insertion_sort(List_ptr list, Matcher is_equal) {
